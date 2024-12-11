d37107f9f9e76cb6c218a256c2ccb4aebfe0d811
weii1501 <vodacluc15@gmail.com>
2024-12-12 00:02:26 +0700

    fix: api test

--- EStudyApp/serializers.py
+++ EStudyApp/serializers.py
@@ -125,7 +125,7 @@ class TestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Test
-        fields = ['id', 'name', 'description', 'test_date', 'duration', 'question_count', 'part_count', 'tag']
+        fields = ['id', 'name', 'description', 'test_date', 'duration', 'question_count', 'part_count', 'tag', 'types']
 
 
 class PartListSerializer(serializers.ModelSerializer):
--- EStudyApp/views.py
+++ EStudyApp/views.py
@@ -286,9 +286,11 @@ class TestListView(APIView):
 
     def get(self, request, format=None):
         # Lấy danh sách bài kiểm tra, tránh truy vấn toàn bộ cơ sở dữ liệu
-        types = request.GET.get('types') if request.GET.get('type') is None else 'Practice'
-        tests = Test.objects.filter(publish=True, types=types).select_related(
+        # get type from request and default is Practice
+        type = request.GET.get('type') if request.GET.get('type') is not None else 'Practice'
+        tests = Test.objects.filter(publish=True, types=type).select_related(
             'tag').order_by('id')  # Sắp xếp theo `id`
+
         paginator = FixedTestPagination()  # Sử dụng phân trang cố định
         paginated_tests = paginator.paginate_queryset(
             tests, request)  # Phân trang dữ liệu
