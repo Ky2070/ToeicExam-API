cc6d09cf00026811e793b06d86811647a9b878c5
weii1501 <vodacluc15@gmail.com>
2024-12-12 01:12:00 +0700

    fix

--- EStudyApp/serializers.py
+++ EStudyApp/serializers.py
@@ -125,7 +125,7 @@ class TestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Test
-        fields = ['id', 'name', 'description', 'test_date', 'duration', 'question_count', 'part_count', 'tag', 'types']
+        fields = ['id', 'name', 'description', 'test_date', 'duration', 'question_count', 'part_count', 'tag', 'types', 'part_test']
 
 
 class PartListSerializer(serializers.ModelSerializer):
