612088a753f9401510816c9176205696e6994944
weii1501 <vodacluc15@gmail.com>
2025-01-02 01:59:32 +0700

    fix resolve

--- EStudyApp/admin.py
+++ EStudyApp/admin.py
@@ -590,10 +590,10 @@ class QuestionAdmin(admin.ModelAdmin):
         'question_text',
         'correct_answer',
         'answers',
-        # 'created_at',
-        # 'update_at',
+        'created_at',
+        'updated_at',
     )
-    # readonly_fields = ('created_at', 'update_at')  # Chỉ đọc thời gian tạo/cập nhật
+    readonly_fields = ('created_at', 'update_at')  # Chỉ đọc thời gian tạo/cập nhật
 
     # 1. Hiển thị nội dung ngắn gọn của câu hỏi
     def short_question_text(self, obj):
