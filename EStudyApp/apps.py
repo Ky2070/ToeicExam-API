794c15c170b0b42d89d112c353db5e11d38f8d1c
Ky2070 <2051052070@ou.edu.vn>
2025-08-19 14:26:07 +0700

    config: docker-compose and cache API with Redis

--- EStudyApp/apps.py
+++ EStudyApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EstudyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EStudyApp'
+
+    def ready(self):
+        import EStudyApp.signals
